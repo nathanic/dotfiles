0e0be740f1f89ea7547c4241b2fc6f16bcbff71a
Nathan Stien <nathanism@gmail.com>
2012-08-31 17:24:43 -0500

    add kibit as a systemwide leiningen plugin

--- config/lein-profiles.clj
+++ config/lein-profiles.clj
@@ -1,2 +1,4 @@
 {:user {:plugins [[lein-swank "1.4.4"]
-                  [lein-pprint "1.1.1"]]}}
+                  [lein-pprint "1.1.1"]
+                  [jonase/kibit "0.0.4"]
+                  ]}}
