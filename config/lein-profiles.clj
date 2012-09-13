268002d97b0ddff04530c17d9191742c78778a21
Nathan Stien <nathanism@gmail.com>
2012-09-13 08:49:54 -0500

    added lein-pedantic plugin

--- config/lein-profiles.clj
+++ config/lein-profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-swank "1.4.4"]
                   [lein-pprint "1.1.1"]
                   [jonase/kibit "0.0.4"]
+                  [lein-pedantic "0.0.3"]
                   ]}}
