89224db01b14ca90cc0383534eb8a6227f67c4a2
Nathan Stien <nathanism@gmail.com>
2012-11-02 21:27:57 -0500

    support lein ritz

--- config/lein-profiles.clj
+++ config/lein-profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-swank "1.4.4"]
+                  [lein-ritz "0.5.0"]   ; improved swank interface
                   [lein-pprint "1.1.1"]
                   [jonase/kibit "0.0.4"]
                   [lein-pedantic "0.0.3"]
