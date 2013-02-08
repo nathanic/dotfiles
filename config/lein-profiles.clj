0a27b0e8e652ce0941aa35e18c5a2a5dcdac3ead
Nathan Stien <npstien@ilstu.edu>
2013-02-08 08:16:03 -0600

    update lein plugins

--- config/lein-profiles.clj
+++ config/lein-profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[lein-swank "1.4.4"]
                   [lein-ritz "0.5.0"]   ; improved swank interface
                   [lein-pprint "1.1.1"]
-                  [jonase/kibit "0.0.4"]
-                  [lein-pedantic "0.0.3"]
+                  [jonase/kibit "0.0.6"]
+                  [lein-pedantic "0.0.5"]
                   ]}}
