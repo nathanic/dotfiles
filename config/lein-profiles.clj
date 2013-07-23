1f86b1ab51e9910409c0c70a7027d90838c01aca
Nathan Stien <nathanism@gmail.com>
2013-07-23 13:08:17 -0500

    lein: remove pedantic from :user profile

--- config/lein-profiles.clj
+++ config/lein-profiles.clj
@@ -2,13 +2,13 @@
                   [lein-ritz "0.7.0" :exclusions [org.clojure/clojure]]   ; improved swank interface
                   [lein-pprint "1.1.1"]
                   [lein-kibit "0.0.7"]
-                  [lein-pedantic "0.0.5"]
                   ]}
 
  ; profile without lein-pedantic
- :fuckit {:plugins [[lein-swank "1.4.5"]
-                    [lein-ritz "0.7.0" :exclusions [org.clojure/clojure]]   ; improved swank interface
-                    [lein-pprint "1.1.1"]
-                    [lein-kibit "0.0.7"]
-                    ]}}
+ :pedantic {:plugins [[lein-swank "1.4.5"]
+                      [lein-ritz "0.7.0" :exclusions [org.clojure/clojure]]   ; improved swank interface
+                      [lein-pprint "1.1.1"]
+                      [lein-kibit "0.0.7"]
+                      [lein-pedantic "0.0.5"]
+                      ]}}
 
