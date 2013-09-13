e7a986b2b0a89f0143d2a2f6620aa5451c1a15e2
Nathan Stien <nathanism@gmail.com>
2013-09-13 16:03:19 -0500

    leiningen: add lein-ancient to default profile

--- config/lein-profiles.clj
+++ config/lein-profiles.clj
@@ -2,6 +2,7 @@
                   [lein-ritz "0.7.0" :exclusions [org.clojure/clojure]]   ; improved swank interface
                   [lein-pprint "1.1.1"]
                   [lein-kibit "0.0.7"]
+                  [lein-ancient "0.4.4"]
                   ]}
 
  ; profile without lein-pedantic
