da08cd1746af2603df967d36c75a8e6004620ee7
Nathan Stien <nathanism@gmail.com>
2013-10-08 23:08:24 -0500

    leiningen: add lein-ancient

--- config/lein-profiles.clj
+++ config/lein-profiles.clj
@@ -1,15 +1,9 @@
 {:user {:plugins [[lein-swank "1.4.5"]
-                  [lein-ritz "0.7.0" :exclusions [org.clojure/clojure]]   ; improved swank interface
+                  [lein-ritz "0.7.0" :exclusions [org.clojure/clojure]]
                   [lein-pprint "1.1.1"]
                   [lein-kibit "0.0.7"]
                   [lein-ancient "0.4.4"]
-                  ]}
-
- ; profile without lein-pedantic
- :pedantic {:plugins [[lein-swank "1.4.5"]
-                      [lein-ritz "0.7.0" :exclusions [org.clojure/clojure]]   ; improved swank interface
-                      [lein-pprint "1.1.1"]
-                      [lein-kibit "0.0.7"]
-                      [lein-pedantic "0.0.5"]
-                      ]}}
+                  [lein-try "0.3.0"]
+                  ;; [lein-pedantic "0.0.5"]
+                  ]}}
 
